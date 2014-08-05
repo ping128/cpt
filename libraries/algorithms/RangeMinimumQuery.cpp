9c37f3308845eaf8471a681da7f694e348c807b9
ping128 <sping128@gmail.com>
2014-08-04 17:20:08 -0700

    Fixed coding style in RMQ

--- libraries/algorithms/RangeMinimumQuery.cpp
+++ libraries/algorithms/RangeMinimumQuery.cpp
@@ -23,14 +23,14 @@ public:
             M[i] = vector<T> (25);
 
         //initialize M for the intervals with length 1
-        for(int i = 0; i < N; i++ ) {
+        for (int i = 0; i < N; i++) {
             M[i][0] = i;
             A[i] = in[i];
         }
         //compute values from smaller to bigger intervals
-        for(int j = 1; 1<<j <= N; j++ ){
-            for(int i = 0; i + (1<<j) - 1 < N; i++ )
-                if(A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]]) M[i][j] = M[i][j - 1];
+        for (int j = 1; 1<<j <= N; j++){
+            for (int i = 0; i + (1<<j) - 1 < N; i++)
+                if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]]) M[i][j] = M[i][j - 1];
                 else M[i][j] = M[i + (1 << (j - 1))][j - 1];
         }
     }
@@ -40,9 +40,9 @@ public:
     int get_min(int left, int right) {
         int num = right - left + 1;
         int e = 1, k = 0;
-        while(e * 2 <= num) e *= 2, k++;
+        while (e * 2 <= num) e *= 2, k++;
         int temp = right - e + 1;
-        if(A[M[left][k]] <= A[M[temp][k]]) return A[M[left][k]];
+        if (A[M[left][k]] <= A[M[temp][k]]) return A[M[left][k]];
         else return A[M[temp][k]];
     }
 };
