09141c954ac87ea5f0804b496c93e505e0088ad6
ping128 <sping128@gmail.com>
2014-10-07 07:20:18 -0700

    Fixed DefaultCode.cpp

--- libraries/DefaultCode.cpp
+++ libraries/DefaultCode.cpp
@@ -5,13 +5,12 @@ using namespace std;
 #define x first
 #define y second
 #define SZ(x) ((int)((x).size()))
-#define PB(x) push_back(x);
+#define PB(x) push_back(x)
 #define INF (0x3f3f3f3f)
-#define MEMSET(x,v) memset(x,v,sizeof(x));
+#define MEMSET(x,v) memset(x,v,sizeof(x))
 
 typedef long long LL;
 typedef pair<int, int> PII; typedef pair<PII, int> PII2;
-typedef vector<int> VI; typedef vector<VI> VVI;
 
 int main() {
     
