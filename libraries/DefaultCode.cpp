f6b2d379c171e6b56874595dc760e04afb0595d9
ping128 <sping128@gmail.com>
2015-01-14 07:44:56 -0800

    Modified code template

--- libraries/DefaultCode.cpp
+++ libraries/DefaultCode.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 #define x first
 #define y second
+#define INF (0x3f3f3f3f)
+
 #define SZ(x) ((int)((x).size()))
 #define PB(x) push_back(x)
-#define INF (0x3f3f3f3f)
 #define MEMSET(x,v) memset(x,v,sizeof(x))
+#define REP(i,n) for(int (i)=0;(i)<(n);++(i))
 
 typedef long long LL;
 typedef pair<int, int> PII; typedef pair<PII, int> PII2;
