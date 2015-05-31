652845e78724de690a01bed423c89bea52a25133
Siwakorn Srisakaokul <sping128@gmail.com>
2015-05-31 12:37:55 -0700

    Updated CentroidDecomposition.cpp

--- library/data_structure/CentroidDecomposition.cpp
+++ library/data_structure/CentroidDecomposition.cpp
@@ -1,41 +1,46 @@
 /////////////////////////////////////////////////////////
 //  Centroid Decomposition
+//  Complexity: N log N
 //  Link: http://codeforces.com/contest/321/problem/C
 /////////////////////////////////////////////////////////
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define SZ(x) ((int)((x).size()))
+#define PB(x) push_back(x)
+#define REP(i,n) for(int (i)=0;(i)<(n);++(i))
 
 class CentroidDecomposition {
-    typedef vector<int> VI;
-    typedef vector<VI> VVI;
 private:
     vector<bool> removed;
-    VI tree_size;
 public:
     int N;
     int cd_root;
-    VI cd_depths, cd_parents;
+    vector<int> cd_depths, cd_parents, tree_size;
+    vector<vector<int>> adj;
     // Vertices are 0-based indexed.
-    CentroidDecomposition (VVI &graph) {
-        N = graph.size(); cd_depths.resize(N); removed.resize(N);
+    CentroidDecomposition (int n) : N(n) {
+        adj.resize(N); cd_depths.resize(N); removed.resize(N);
         tree_size.resize(N); cd_parents.resize(N);
-        cd_root = decompose(0, 0, graph); cd_parents[cd_root] = -1;
+    }
+    void add_edge(int u, int v) {
+        adj[u].PB(v); adj[v].PB(u);
+    }
+    void decompose() {
+        cd_root = decompose(0, 0);
+        cd_parents[cd_root] = -1;
     }
 private:
     // Returns the centroid of the current subtree
-    int decompose(int at, int dep, VVI &adj) {
-        int center = find_center(at, adj);
-        cd_depths[center] = dep; removed[center] = true;
-        int sz = adj[center].size();
-        for (int i = 0; i < sz; i++) {
-            int v = adj[center][i];
+    int decompose(int at, int dep) {
+        int center = find_center(at);
+        cd_depths[center] = dep;
+        removed[center] = true;
+        for (int v : adj[center]) {
             if (!removed[v]) {
-                int subroot = decompose(v, dep + 1, adj);
+                int subroot = decompose(v, dep + 1);
                 cd_parents[subroot] = center;
             }
         }
@@ -43,47 +48,44 @@ private:
     }
 
     // Center node is the node whose all subtrees' size is not greater than half of the whole tree
-    int find_center(int at, VVI &adj) {
-        int par = -1; cal_size(at, -1, adj);
-        int limit = tree_size[at] / 2;
-        while (1) {
-            bool found = true; int sz = adj[at].size();
-            for (int i = 0; i < sz; i++) {
-                int v = adj[at][i];
+    int find_center(int at) {
+        cal_size(at, -1);
+        int limit = tree_size[at] / 2, par = -1;
+        bool found = false;
+        while (!found) {
+            found = true;
+            for (int v : adj[at]) {
                 if (!removed[v] && v != par && tree_size[v] > limit) {
-                    found = false; par = at; at = v; break;
+                    found = false;
+                    par = at, at = v;
+                    break;
                 }
             }
-            if (found) return at;
         }
+        return at;
     }
 
-    void cal_size(int at, int par, VVI &adj) {
-        tree_size[at] = 1; int sz = adj[at].size();
-        for (int i = 0; i < sz; i++) {
-            int v = adj[at][i];
-            if (!removed[v] && par != v) {
-                cal_size(v, at, adj); tree_size[at] += tree_size[v];
+    void cal_size(int at, int par) {
+        tree_size[at] = 1;
+        for (int v : adj[at]) {
+            if (v != par && !removed[v]) {
+                cal_size(v, at);
+                tree_size[at] += tree_size[v];
             }
         }
     }
 };
 
-typedef vector<int> VI;
-typedef vector<VI> VVI;
-
 int main(){
     int N;
-    VVI adj;
     int u, v;
     scanf("%d", &N);
-    adj.resize(N);
+    CentroidDecomposition cd(N);
     for(int i = 0; i < N - 1; i++ ){
         scanf("%d %d", &u, &v); u--, v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        cd.add_edge(u, v);
     }
-    CentroidDecomposition cd(adj);
+    cd.decompose();
     for (int i = 0; i < N; i++) {
         printf("%c ", cd.cd_depths[i] + 'A');
     }
