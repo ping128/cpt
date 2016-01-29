a8ab3c9875bbbb8e5f14218f60d4fbd8a43e8a8f
Siwakorn Srisakaokul <sping128@gmail.com>
2016-01-29 00:39:39 -0600

    Update Hungarian.cpp

--- library/algorithms/Hungarian.cpp
+++ library/algorithms/Hungarian.cpp
@@ -33,24 +33,18 @@ public:
                 for (int j = 1; j <= m; j++) {
                     if (!used[j]) {
                         int cur = cost[i0 - 1][j - 1] - u[i0] - v[j];
-                        if (cur < minv[j])
-                            minv[j] = cur, way[j] = j0;
-                        if (minv[j] < delta)
-                            delta = minv[j], j1 = j;
+                        if (cur < minv[j]) minv[j] = cur, way[j] = j0;
+                        if (minv[j] < delta) delta = minv[j], j1 = j;
                     }
                 }
                 for (int j = 0; j <= m; j++) {
-                    if (used[j])
-                        u[p[j]] += delta, v[j] -= delta;
-                    else
-                        minv[j] -= delta;
+                    if (used[j]) u[p[j]] += delta, v[j] -= delta;
+                    else minv[j] -= delta;
                 }
                 j0 = j1;
             } while (p[j0] != 0);
             do {
-                int j1 = way[j0];
-                p[j0] = p[j1];
-                j0 = j1;
+                int j1 = way[j0]; p[j0] = p[j1]; j0 = j1;
             } while (j0);
         }
         min_cost = -v[0];
