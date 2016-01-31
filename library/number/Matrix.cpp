5b6967ff790f6ccfe462b52ea31296a3ce3d4dd7
Siwakorn Srisakaokul <sping128@gmail.com>
2016-01-31 13:06:27 -0600

    Update Matrix.cpp

--- library/number/Matrix.cpp
+++ library/number/Matrix.cpp
@@ -11,70 +11,56 @@ using namespace std;
 template<class T>
 class Matrix {
 public:
-    vector<vector<T> > mat;
+    vector<vector<T>> a;
     int N, M;
-    
+    Matrix() {}
     Matrix (int n, int m) {
         N = n;
         M = m;
-        mat = vector<vector<T> >(N);
+        a = vector<vector<T>>(N);
         for (int i = 0; i < N; i++)
-            mat[i] = vector<T> (M);
+            a[i] = vector<T>(M);
     }
-
-    Matrix mult(const Matrix &a) {
-        assert(M == a.N);
-        Matrix ret = Matrix(N, a.M);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < a.M; j++) {
-                for (int k = 0; k < M; k++) {
-                    ret.mat[i][j] += mat[i][k] * a.mat[k][j];
-                }
-            }
-        }
+    Matrix mult(const Matrix &o) {
+        assert(M == o.N);
+        Matrix ret = Matrix(N, o.M);
+        REP(i, N) REP(j, o.M) REP(k, M)
+            ret[i][j] += a[i][k] * o.a[k][j];
         return ret;
     }
-
     Matrix power(long long ex) {
         assert(N == M);
-        Matrix ret = identity(N), b = *this;
+        Matrix ret = identity(), b = *this;
         while (ex) { if (ex & 1) ret = ret.mult(b); b = b.mult(b); ex >>= 1; }
         return ret;
     }
-
-    Matrix identity(int n) {
-        Matrix ret = Matrix(n, n);
-        for (int i = 0; i < n; i++) ret.mat[i][i] = 1;
+    Matrix identity() {
+        assert(N == M);
+        Matrix ret = Matrix(N, N);
+        REP(i, N) ret[i][i] = 1;
         return ret;
     }
-    
-    void print() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                cout << mat[i][j] << " ";
-            }
-            cout << endl;
-        }
+    vector<T> &operator[](int ind) {
+        assert(ind >= 0 && ind < N);
+        return a[ind];
+    }
+    inline friend ostream& operator << (ostream &out, Matrix m) {
+        REP(i, m.N) { out << "|"; REP(j, m.M) out << " " << m.a[i][j]; out << " |" << endl; }
+        return out;
     }
 };
-    
 
 int main() {
-    
     Matrix<int> start = Matrix<int>(2, 2);
-    start.mat[0][1] = start.mat[1][0] = start.mat[1][1] = 1;
-
-    start.print();
+    start[0][1] = start[1][0] = start[1][1] = 1;
+    cout << start << endl;
     Matrix<int> now = start;
     for (int i = 1; i < 10; i++) {
-        now.print();
+        cout << now << endl;
         now = now.mult(start);
     }
-
     for (int i = 1; i < 10; i++) {
-        start.power(i).print();
+        cout << start.power(i) << endl;
     }
-        
-    
     return 0;
 }
