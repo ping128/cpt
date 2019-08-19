f247a96c4811046a0096d73c29c0dd34e9d1e7a7
Siwakorn Srisakaokul <sping128@gmail.com>
2019-08-18 22:02:07 -0700

    RMQ

--- library/algorithms/RangeMinimumQuery.cpp
+++ library/algorithms/RangeMinimumQuery.cpp
@@ -9,16 +9,15 @@
 
 using namespace std;
 
-template<class T>
 class RangeMinimumQuery {
 public:
     vector<vector<int> > M;
-    vector<T> A;
+    vector<int> A;
     int N;
-    RangeMinimumQuery(T in[], int n) {
-        N = n;
+    RangeMinimumQuery(const vector<int>& in) {
+        N = in.size();
         M = vector<vector<int> >(N + 5);
-        A = vector<T> (N + 5);
+        A = vector<int> (N + 5);
         int b; for (b = 0; 1<<b <= N; b++);
         for (int i = 0; i < N + 5; i++)
             M[i] = vector<int> (b);
@@ -38,7 +37,7 @@ public:
 
     // returns min(A[left...right])
     // also can be modified to return the index
-    T get_min(int left, int right) {
+    int get_min(int left, int right) {
         int num = right - left + 1;
         int e = 1, k = 0;
         while (e * 2 <= num) e *= 2, k++;
@@ -48,7 +47,7 @@ public:
     }
 };
 
-int bruteforce_rmq(int *a, int left, int right) {
+int bruteforce_rmq(vector<int>& a, int left, int right) {
     int ret = a[left];
     for (int i = left + 1; i <= right; i++) {
         ret = min(ret, a[i]);
@@ -58,21 +57,13 @@ int bruteforce_rmq(int *a, int left, int right) {
 
 int main() {
     int N = 15;
-    int in[] = {-1, 4, 1, 6, 2, -10, 11, 3, 7, 100, 4, -123123123, 19, 1, -5};
-    RangeMinimumQuery<int> rmq(in, N);
+    vector<int> in = {-1, 4, 1, 6, 2, -10, 11, 3, 7, 100, 4, -123123123, 19, 1, -5};
+    RangeMinimumQuery rmq(in);
 
     for (int i = 0; i < N; i++) {
         for (int j = i; j < N; j++) {
             assert(rmq.get_min(i, j) == bruteforce_rmq(in, i, j));
         }
     }
-    /*
-    assert(rmq.get_min(0, 0) == -1);
-    assert(rmq.get_min(0, 9) == -10);
-    assert(rmq.get_min(0, 2) == -1);
-    assert(rmq.get_min(4, 9) == -10);
-    assert(rmq.get_min(3, 3) == 6);
-    assert(rmq.get_min(6, 8) == 3);
-    */
     return 0;
 }
